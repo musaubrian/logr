deeeb42828cfe5f83acb3ec3c370fd71d4e2f116
musaubrian <musaubrian15@gmail.com>
2024-10-28 11:15:20 +0300

    feat: show file and line no of caller
    - update docs to show structure of log

--- README.md
+++ README.md
@@ -6,9 +6,12 @@ This is very much not to be used by anyone other than me.
 But if you want to try it out:
 
 ```go
+// [LEVEL] <file:line_no> <time:optional> <message>
 
 //...
 log := logr.New(logr.LevelInfo)
+// Defaults to time in the format: 15:04:06
+
 log.Info("Hello world")
 log.Debug("Hello world")
 log.Warn("Hello world")
@@ -20,6 +23,6 @@ log.Logf(logr.LevelInfo,"Some value %d", 45+90)
 
 ## TODO
 
-- [ ] Align logs
+- [x] Align logs
 - [ ] save to file?
 
--- logr.go
+++ logr.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"runtime"
 	"strconv"
+	"strings"
 	"time"
 )
 
 type LogLevel int
 
 const (
-	LevelInfo LogLevel = iota
+	LevelInfo LogLevel = iota + 1
 	LevelDebug
 	LevelWarn
 	LevelError
@@ -26,13 +29,18 @@ const (
 	darkGray  = 90
 	dimmed    = 2
 
-	labelWidth = 7
+	labelWidth = 8
+	fileWidth  = 20
+
+	skip = 3
 )
 
 type Logr struct {
 	*log.Logger
 	level      LogLevel
 	timeFormat string
+	showFile   bool
+	saveToFile bool
 }
 
 func New(minLevel LogLevel) *Logr {
@@ -40,32 +48,53 @@ func New(minLevel LogLevel) *Logr {
 		Logger:     log.New(os.Stdout, "", 0),
 		level:      minLevel,
 		timeFormat: "",
+		saveToFile: false,
 	}
 }
 
-func (l *Logr) WithTime() *Logr {
+// Pass in a time format else defaults to `2006-01-02 15:04:05`
+func (l *Logr) WithTime(format ...string) *Logr {
+	if len(format) == 0 {
+		format = append(format, "2006-01-02 15:04:05")
+	}
+
 	return &Logr{
 		Logger:     l.Logger,
 		level:      l.level,
-		timeFormat: "15:04:05",
+		timeFormat: format[0],
+		saveToFile: l.saveToFile,
 	}
 }
 
-func bold(v string) string {
-	return fmt.Sprintf("\033[1m%s", v)
+func (l *Logr) WithFile() *Logr {
+	return &Logr{
+		Logger:     l.Logger,
+		level:      l.level,
+		timeFormat: l.timeFormat,
+		saveToFile: false,
+	}
 }
 
-func colorize(colorCode int, v string) string {
-	return fmt.Sprintf("\033[%sm%s%s", strconv.Itoa(colorCode), v, reset)
-}
 func (l *Logr) log(level LogLevel, colorCode int, label string, msg string) {
+	if level < l.level {
+		return
+	}
+
 	timeStr := ""
 	if l.timeFormat != "" {
-		timeStr = fmt.Sprintf("%s", time.Now().Format(l.timeFormat))
+		timeStr = time.Now().Format(l.timeFormat)
 	}
-	if level >= l.level {
-		l.Printf("%s %s %s", bold(colorize(colorCode, label)), colorize(darkGray, timeStr), msg)
-	}
+
+	paddedLabel := padLabel(label)
+	paddedTime := l.padTime(timeStr)
+
+	fileStr := getFileInfo()
+
+	l.Printf("%s%s %s%s",
+		bold(colorize(colorCode, paddedLabel)),
+		colorize(darkGray, fileStr),
+		colorize(darkGray, paddedTime),
+		msg)
 }
 
 func (l *Logr) Info(msg string) {
@@ -75,17 +104,20 @@ func (l *Logr) Info(msg string) {
 func (l *Logr) Debug(msg string) {
 	l.log(LevelDebug, dimmed, "[DEBUG]", msg)
 }
+
 func (l *Logr) Warn(msg string) {
 	l.log(LevelWarn, yellow, "[WARN]", msg)
 }
+
 func (l *Logr) Error(msg string) {
 	l.log(LevelError, red, "[ERROR]", msg)
 }
 
-// Used the same as log.Fatalf
-//
-//	logr.Logf(logr.LevelInfo, "Random: %v", []string{"Hello", "world"})
 func (l *Logr) Logf(level LogLevel, format string, v ...any) {
+	if level < l.level {
+		return
+	}
+
 	var colorCode int
 	var label string
 
@@ -104,12 +136,58 @@ func (l *Logr) Logf(level LogLevel, format string, v ...any) {
 
 	timeStr := ""
 	if l.timeFormat != "" {
-		timeStr = " " + time.Now().Format(l.timeFormat) + " "
+		timeStr = time.Now().Format(l.timeFormat)
 	}
 
-	if level >= l.level {
-		l.Printf("%s%s  %s", bold(colorize(colorCode, label)),
-			colorize(darkGray, timeStr),
-			fmt.Sprintf(colorize(lightGray, format), v...))
+	paddedLabel := padLabel(label)
+	paddedTime := l.padTime(timeStr)
+
+	l.Printf("%s%s%s",
+		bold(colorize(colorCode, paddedLabel)),
+		colorize(darkGray, paddedTime),
+		fmt.Sprintf(colorize(lightGray, format), v...))
+}
+
+func getFileInfo() string {
+	_, file, line, ok := runtime.Caller(skip)
+	if !ok {
+		return "???:0"
 	}
+	file = filepath.Base(file)
+	return fmt.Sprintf("%s:%d", file, line)
+}
+
+func bold(v string) string {
+	return fmt.Sprintf("\033[1m%s", v)
+}
+
+func colorize(colorCode int, v string) string {
+	return fmt.Sprintf("\033[%sm%s%s", strconv.Itoa(colorCode), v, reset)
+}
+
+func padLabel(label string) string {
+	visibleLen := len(label)
+	padding := strings.Repeat(" ", (len(label)+1)-visibleLen)
+	return label + padding
+}
+
+func (l *Logr) padTime(timeStr string) string {
+	if timeStr == "" {
+		return ""
+	}
+
+	padding := strings.Repeat(" ", (len(l.timeFormat)+2)-len(timeStr))
+	return timeStr + padding
+}
+
+func (l *Logr) padFile(file string) string {
+	if file == "" {
+		return ""
+	}
+	// -3 to account for "..."
+	if len(file) > fileWidth-3 {
+		file = "..." + file[len(file)-(fileWidth-3):]
+	}
+	padding := strings.Repeat(" ", fileWidth-len(file))
+	return file + padding
 }
